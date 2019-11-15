ecf2774580868bc9de878ace0414719dc02fcce6
Jens Theeß <theess@subshell.com>
2019-11-15 20:11:37 +0100

    Revert "copy database only if the external database file changed"
    This reverts commit 696a0eb6

--- app/src/main/java/de/theess/eisbaer/data/Database.kt
+++ app/src/main/java/de/theess/eisbaer/data/Database.kt
@@ -6,9 +6,6 @@ import android.content.SharedPreferences
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.net.Uri
-import android.provider.OpenableColumns
-import androidx.core.content.edit
-import androidx.core.database.getIntOrNull
 import androidx.preference.PreferenceManager
 import de.theess.eisbaer.BuildConfig
 import de.theess.eisbaer.EisbaerApplication
@@ -21,7 +18,7 @@ import java.io.FileOutputStream
 /**
  * Manages the requery data store.
  */
-class Database(private val context: Context) {
+class Database(val context: Context) {
 
     /**
      * Open the database once to initialize the android_metadata table.
@@ -41,33 +38,20 @@ class Database(private val context: Context) {
     val entityStoreHolder: EntityStoreHolder = EntityStoreHolder(null)
 
     /**
-     * Size of the external database. This value is persisted to the preferences.
-     */
-    private var dbFileSize: Int = 0
-        set(value) {
-            field = value
-            PreferenceManager.getDefaultSharedPreferences(context).edit {
-                putInt(EisbaerApplication.PREF_DATABASE_FILE_SIZE, value)
-            }
-        }
-
-    /**
      * Resets the holder if the database uri pref changes.
      */
     private val preferenceListener: SharedPreferences.OnSharedPreferenceChangeListener =
-        SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+        SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
             if (key == EisbaerApplication.PREF_DATABASE_URI) synchronized(this) {
                 Timber.d("database uri pref changed")
-                updateDatabase()
+                initEntityStore()
             }
         }
 
     init {
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        preferences.registerOnSharedPreferenceChangeListener(preferenceListener)
-        dbFileSize = preferences.getInt(EisbaerApplication.PREF_DATABASE_FILE_SIZE, 0)
-        updateDatabase()
-        if (!isOpen()) openDatabase()
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .registerOnSharedPreferenceChangeListener(preferenceListener)
+        initEntityStore()
     }
 
     /**
@@ -75,62 +59,21 @@ class Database(private val context: Context) {
      * database is copied into the application data directory and then opened.
      */
     @Synchronized
-    private fun updateDatabase() {
+    private fun initEntityStore() {
         val uri = getExternalDatabaseUri(context)
         if (uri == null || !checkUriGrant(context, uri)) {
-            Timber.i("No database file uri.")
+            Timber.i("No database file")
             return
         }
-
-        if (context.getDatabasePath(DATABASE_NAME).exists()
-            && !externalDatabaseChanged(context, uri)
-        ) {
-            Timber.d("Database has not changed.")
-            return
-        }
-
-        Timber.d("Database changed.")
 
         copyDatabase(context, uri)
         DbInitializer(context, DATABASE_NAME).initDatabase()
-        openDatabase()
-    }
 
-    /**
-     * Opens the internal database.
-     */
-    private fun openDatabase() {
         val source = DatabaseSource(context, Models.DEFAULT, DATABASE_NAME, 1)
         if (BuildConfig.DEBUG) {
             source.setLoggingEnabled(true)
         }
-        entityStoreHolder.store = KotlinEntityDataStore(source.configuration)
-    }
-
-    private fun isOpen(): Boolean {
-        return entityStoreHolder.store != null
-    }
-
-    /**
-     * Checks whether the external database file has changed.
-     */
-    private fun externalDatabaseChanged(context: Context, uri: Uri): Boolean {
-        Timber.d("Checking database.")
-
-        val cursor = context.contentResolver.query(uri, null, null, null, null)
-
-        cursor?.use {
-            // moveToFirst() returns false if the cursor has 0 rows.
-            if (it.moveToFirst()) {
-                val size: Int = it.getIntOrNull(it.getColumnIndex(OpenableColumns.SIZE)) ?: 0
-                Timber.d("size: %d, previous size: %d", size, dbFileSize)
-                val changed = size != dbFileSize
-                dbFileSize = size
-                return changed
-            }
-        }
-
-        return true
+        entityStoreHolder.store = KotlinEntityDataStore<Any>(source.configuration)
     }
 
     /**
@@ -153,25 +96,19 @@ class Database(private val context: Context) {
         Timber.d("copyDatabase done")
     }
 
-    /**
-     * Check whether we still can access the database uri.
-     */
     private fun checkUriGrant(context: Context, uri: Uri): Boolean {
-        return try {
+        try {
             context.contentResolver.takePersistableUriPermission(
                 uri,
                 Intent.FLAG_GRANT_READ_URI_PERMISSION
             )
-            true
+            return true
         } catch (e: Exception) {
             Timber.d(e, "Failed to get uri grant.")
-            false
+            return false
         }
     }
 
-    /**
-     * Gets the URI to the external database file from the preferences.
-     */
     private fun getExternalDatabaseUri(context: Context): Uri? {
         return PreferenceManager.getDefaultSharedPreferences(context)
             .getString(EisbaerApplication.PREF_DATABASE_URI, null)
@@ -179,9 +116,6 @@ class Database(private val context: Context) {
     }
 
     companion object {
-        /**
-         * Name of the internal database.
-         */
         private const val DATABASE_NAME = "note_db"
     }
 }
